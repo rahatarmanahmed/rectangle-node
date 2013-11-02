bb2dce9af63b8d9c3ae974378e44474d7f550908
Rahat Ahmed <rahatarmanahmed@gmail.com>
2013-11-01 23:57:05 -0500

    Initial commit.
    Not all functions finished yet.

---
+++ index.js
@@ -0,0 +1,295 @@
+// # Rectangle
+// An object representing a rectangle.
+// This object has four properties: x, y, w, h.
+// x and y represent the top left corner of this rectangle.
+// w and h represent this rectangle's width and height respectively.
+// The positive y axis is assumed to point down.
+// Allows negative widths and heights, but its highly unrecommended to use them.
+
+// Written by [Rahat Ahmed](http://rahatah.me/d).
+
+var Vector2 = require('vector2-node');
+
+// TODO
+// ----
+//contains rect/vec2
+
+//aspectRatio get/set(same area)
+
+//area get/set(same ratio)
+
+//overlaps rect, x,y,w,h
+
+//diagonal get/set?
+
+// topLeft topRight bottomLeft bottomRight?
+
+// equivalent (negative size dont matter yo)
+
+// ## Rectangle
+// ### Rectangle(Rectangle)
+// ### Rectangle(Vector2, Vector2)
+// ### Rectangle(Vector2, w, h)
+// ### Rectangle(x, y, w, h)
+// Constructor for Rectangle
+function Rectangle(x, y, w, h)
+{
+	this.set(x, y, w ,h);
+}
+
+// ## set
+// ### set(Rectangle)
+// ### set(Vector2, Vector2)
+// ### set(Vector2, w, h)
+// ### set(x, y, w, h)
+// Sets this rectangle to the given values.
+
+Rectangle.prototype.set = function(x, y, w, h)
+{
+	if(x instanceof Rectangle)
+	{
+		this.x = x.x;
+		this.y = x.y;
+		this.w = x.w;
+		this.h = x.h;
+	}
+	else if(x instanceof Vector2)
+	{
+		this.x = x.x;
+		this.y = x.y;
+		if(y instanceof Vector2)
+		{
+			this.w = y.x;
+			this.h = y.y;
+		}
+		else
+		{
+			this.w = y;
+			this.h = w;
+		}
+	}
+	else
+	{
+		this.x = x || 0;
+		this.y = y || 0;
+		this.w = w || 0;
+		this.h = h || 0;
+	}
+	return this;
+};
+
+// ## position
+// ### position(Rectangle)
+// Sets this rectangle's x and y to the same x and y of the
+// given Rectangle.
+// Returns **this Rectangle** for chaining.
+// ### position(Vector2)
+// Sets this rectangle's x and y to that of the given Vector2.
+// Returns **this Rectangle** for chaining.
+// ### position(x, y)
+// Sets this rectangle's x and y to that of the given x and y values.
+// Returns **this Rectangle** for chaining.
+// ### position()
+// Returns the **position** Vector2 containing this Rectangle's
+// x and y values.
+Rectangle.prototype.position = function(x, y) {
+	if(x instanceof Rectangle || x instanceof Vector2)
+	{
+		this.x = x.x;
+		this.y = x.y;
+		return this;
+	}
+	else if(x !== undefined)
+	{
+		this.x = x;
+		this.y = y;
+		return this;
+	}
+	return new Vector2(this.x, this.y);
+};
+
+// ## size
+// ### size(Rectangle)
+// Sets this rectangle's w and h to the same w and h of the
+// given Rectangle.
+// Returns **this Rectangle** for chaining.
+// ### size(Vector2)
+// Sets this rectangle's w and h to that of the given Vector2.
+// Returns **this Rectangle** for chaining.
+// ### size(w, h)
+// Sets this rectangle's w and h to that of the given w and h values.
+// Returns **this Rectangle** for chaining.
+// ### size()
+// Returns the **size** Vector2 containing this Rectangle's
+// w and h values.
+Rectangle.prototype.size = function(w, h) {
+	if(w instanceof Rectangle)
+	{
+		this.w = w.w;
+		this.h = w.h;
+		return this;
+	}
+	if(w instanceof Vector2)
+	{
+		this.w = w.x;
+		this.h = w.y;
+		return this;
+	}
+	else if(w !== undefined)
+	{
+		this.w = w;
+		this.h = h;
+		return this;
+	}
+	// TODO: account for negative size
+	return new Vector2(this.w, this.h);
+};
+
+// ## top()
+// Returns the top bound of this Rectangle.
+Rectangle.prototype.top = function() {
+	return (this.h >= 0) ? this.y : this.y + this.h;
+};
+
+// ## bottom()
+// Returns the bottom bound of this Rectangle.
+Rectangle.prototype.bottom = function() {
+	return (this.h >= 0) ? this.y + this.h : this.y;
+};
+
+// ## left()
+// Returns the left bound of this Rectangle.
+Rectangle.prototype.left = function() {
+	return (this.w >= 0) ? this.x : this.x + this.w;
+};
+
+// ## right()
+// Returns the right bound of this Rectangle.
+Rectangle.prototype.right = function() {
+	return (this.h >= 0) ? this.x + this.w : this.x;
+};
+
+// ## copy()
+// ## clone()
+// Returns a new identical Rectangle
+Rectangle.prototype.copy = Rectangle.prototype.clone = function() {
+	return new Rectangle(this.x, this.y, this.w, this.h);
+};
+
+// ## isStandard()
+// A standard Rectangle has a non-negative width and height.
+// Returns true if both the width and height of this Rectangle are non-negative
+Rectangle.prototype.isStandard = function() {
+	return this.w >= 0 && this.h >= 0;
+};
+
+// ## makeStandard()
+// If this Rectangle is non-standard, makes width/height positive
+// and adjusts x and y accordingly. Will not affect a standard
+// Rectangle.
+// Returns this Rectangle for chaining.
+Rectangle.prototype.makeStandard = function() {
+	if(this.w < 0)
+	{
+		this.x += this.w;
+		this.w *= -1;
+	}
+	if(this.h < 0)
+	{
+		this.y += this.h;
+		this.h *= -1;
+	}
+	return this;
+};
+
+// ## translate(Vector2)
+// ## translate(x, y)
+// Translates this Rectangle's position by the given coordinates,
+// relative to its current position.
+// Returns this Rectangle for chaining.
+Rectangle.prototype.translate = function(x, y) {
+	if(x instanceof Vector2)
+	{
+		this.x += x.x;
+		this.y += x.y;
+		return this;
+	}
+	this.x += x;
+	this.y += y;
+	return this;
+};
+
+// ## scale(Vector2)
+// ## scale(x, y)
+// Scales this Rectangle's size by the given scales.
+// Returns this Rectangle for chaining.
+Rectangle.prototype.scale = function(x, y) {
+	if(x instanceof Vector2)
+	{
+		this.w *= x.x;
+		this.h *= x.y;
+		return this;
+	}
+	this.w *= x;
+	this.h *= y;
+	return this;
+};
+
+// ## equals
+// Note: If two rectangles are identical but have different x,y,w,h because
+// of negative sizes, they are **not** equal! If you want to check equality
+// regardless of negative sizes, use .copy().makeStandard().equals()
+// ### equals(Rectangle)
+// Returns true if this and another Rectangle are equal.
+// ### equals(Rectangle, epsilon)
+// Returns true if this and another Rectangle are equal within an epsilon.
+// ### equals(Vector2, Vector2)
+// Returns true if this Rectangle's position matches the first Vector2
+// and its size matches the second Vector2
+// ### equals(Vector2, Vector2, epsilon)
+// Returns true if this Rectangle's position matches the first Vector2
+// and its size matches the second Vector2, within an epsilon
+// ### equals(Vector2, w, h)
+// Returns true if this Rectangle's position matches the Vector2,
+// and its width and height match w and h respectively.
+// ### equals(Vector2, w, h, epsilon)
+// Returns true if this Rectangle's position matches the Vector2,
+// and its width and height match w and h respectively within an epsilon
+// ### equals(x, y, w, h)
+// Returns true if this Rectangle's values matches the ones given.
+// ### equals(x, y, w, h, epsilon)
+// Returns true if this Rectangle's values matches the ones given
+// within an epsilon.
+Rectangle.prototype.equals = function(x, y, w, h, epsilon) {
+	if(x instanceof Rectangle)
+		return this.position().equals(x.position(), y) && this.size().equals(x.size(), y);
+	else if(x instanceof Vector2)
+	{
+		if(y instanceof Vector2)
+			return this.position().equals(x, w) && this.size().equals(y, w);
+		else
+			return this.position().equals(x, h) && this.size().equals(y, w, h);
+	}
+	return this.position().equals(x, y, epsilon) && this.size().equals(w, h, epsilon);
+};
+
+// ## perimeter()
+// Returns the perimeter of this Rectangle.
+Rectangle.prototype.perimeter = function() {
+	return 2 * Math.abs(this.w) + 2 * Math.abs(this.h);
+};
+
+// ## center()
+// Returns a Vector2 of the center of this Rectangle
+// ## center(Vector2)
+// ## center(x, y)
+// Sets this Rectangle's position so that the input
+// is where the Rectangle's center is.
+Rectangle.prototype.center = function(x, y) {
+	if(x instanceof Vector2)
+		return this.position(x.x - this.w / 2,  x.y - this.h / 2);
+	else if(x !== undefined)
+		return this.position(x - this.w / 2, this.y = y - this.h / 2);
+	return new Vector2(this.x + this.w / 2, this.y + this.h / 2);
+};
+module.exports = Rectangle;
---
+++ test/rectangleSpec.js
@@ -0,0 +1,209 @@
+var Rectangle = require('../index.js'),
+	Vector2 = require('vector2-node');
+var epsilon = 0.0000000001;
+
+describe("Rectangle test suite:", function() {
+	var a, b, c, d, e, f;
+	beforeEach(function() {
+		// Rectangle matcher
+		this.addMatchers({
+			toBeRectangle : function(x, y, w, h) {
+				return this.actual.equals(x, y, w, h, epsilon);
+			},
+			toBeStandardRectangle : function(x, y, w, h) {
+				return this.actual.copy().makeStandard().equals(x, y, w, h, epsilon);
+			},
+			toBeVector: function(x, y) {
+				return this.actual.equals(x, y, epsilon);
+			}
+		});
+		// Arbitrary test values
+		a = new Rectangle();
+		b = new Rectangle(1, 2, 3, 4);
+		c = new Rectangle(new Rectangle(-2, 3, 2, 2));
+		d = new Rectangle(new Vector2(1, 2), new Vector2(3, 4));
+		e = new Rectangle(new Vector2(-1, -2), 3, 4);
+		f = new Rectangle(4, 6, -3, -4);
+	});
+
+	it("constructor/set", function() {
+		expect(a).toBeRectangle(0, 0, 0, 0);
+		expect(b).toBeRectangle(1, 2, 3, 4);
+		expect(c).toBeRectangle(-2, 3, 2, 2);
+		expect(d).toBeRectangle(1, 2, 3, 4);
+		expect(e).toBeRectangle(-1, -2, 3, 4);
+		expect(f).toBeRectangle(4, 6, -3, -4);
+	});
+
+	describe("position", function() {
+		it("get", function() {
+			expect(a.position()).toBeVector(0, 0);
+			expect(b.position()).toBeVector(1, 2);
+			expect(c.position()).toBeVector(-2, 3);
+			expect(d.position()).toBeVector(1, 2);
+			expect(e.position()).toBeVector(-1, -2);
+			expect(f.position()).toBeVector(4, 6);
+		});
+
+		it("set", function() {
+			expect(a.position(1, 2)).toBeRectangle(1, 2, 0, 0);
+			expect(a.position(new Vector2(-3, 4))).toBeRectangle(-3, 4, 0, 0);
+			expect(a.position(b)).toBeRectangle(1, 2, 0, 0);
+		});
+	});
+
+	describe("size", function() {
+		it("get", function() {
+			expect(a.size()).toBeVector(0, 0);
+			expect(b.size()).toBeVector(3, 4);
+			expect(c.size()).toBeVector(2, 2);
+			expect(d.size()).toBeVector(3, 4);
+			expect(e.size()).toBeVector(3, 4);
+			expect(f.size()).toBeVector(-3, -4);
+		});
+
+		it("set", function() {
+			expect(a.size(5, 6)).toBeRectangle(0, 0, 5, 6);
+			expect(a.size(new Vector2(-3, 4))).toBeRectangle(0, 0, -3, 4);
+			expect(a.size(b)).toBeRectangle(0, 0, 3, 4);
+		});
+	});
+
+	describe("top", function() {
+		it("get", function() {
+			expect(a.top()).toBe(0);
+			expect(b.top()).toBe(2);
+			expect(c.top()).toBe(3);
+			expect(d.top()).toBe(2);
+			expect(e.top()).toBe(-2);
+			expect(f.top()).toBe(2);
+		});
+	});
+
+	describe("bottom", function() {
+		it("get", function() {
+			expect(a.bottom()).toBe(0);
+			expect(b.bottom()).toBe(6);
+			expect(c.bottom()).toBe(5);
+			expect(d.bottom()).toBe(6);
+			expect(e.bottom()).toBe(2);
+			expect(f.bottom()).toBe(6);
+		});
+	});
+
+	describe("left", function() {
+		it("get", function() {
+			expect(a.left()).toBe(0);
+			expect(b.left()).toBe(1);
+			expect(c.left()).toBe(-2);
+			expect(d.left()).toBe(1);
+			expect(e.left()).toBe(-1);
+			expect(f.left()).toBe(1);
+		});
+	});
+
+	describe("right", function() {
+		it("get", function() {
+			expect(a.right()).toBe(0);
+			expect(b.right()).toBe(4);
+			expect(c.right()).toBe(0);
+			expect(d.right()).toBe(4);
+			expect(e.right()).toBe(2);
+			expect(f.right()).toBe(4);
+		});
+	});
+
+	it("copy/clone", function(){
+		expect(b.copy()).toBeRectangle(1, 2, 3, 4);
+		expect(b.clone()).toBeRectangle(1, 2, 3, 4);
+		var z = b.clone().set(666, 666, 666, 666);
+		expect(b).toBeRectangle(1, 2, 3, 4);
+		expect(z).toBeRectangle(666, 666, 666, 666);
+	});
+
+	it("isStandard", function() {
+		expect(a.isStandard()).toBeTruthy();
+		expect(b.isStandard()).toBeTruthy();
+		expect(c.isStandard()).toBeTruthy();
+		expect(d.isStandard()).toBeTruthy();
+		expect(e.isStandard()).toBeTruthy();
+		expect(f.isStandard()).toBeFalsy();
+	});
+
+	it("makeStandard", function() {
+		expect(a.makeStandard()).toBeRectangle(0, 0, 0, 0);
+		b.makeStandard();
+		expect(b).toBeRectangle(1, 2, 3, 4);
+		expect(f).toBeStandardRectangle(1, 2, 3, 4);
+	});
+
+	it("translate", function() {
+		a.translate(100, -100);
+		expect(a).toBeRectangle(100, -100, 0, 0);
+		expect(b.translate(5, 5)).toBeRectangle(6, 7, 3, 4);
+		expect(f.translate(5, 5)).toBeStandardRectangle(6, 7, 3, 4);
+
+	});
+
+	it("scale", function() {
+		a.scale(100, -100);
+		expect(a).toBeRectangle(0, 0, 0, 0);
+		expect(b.scale(5, 5)).toBeRectangle(1, 2, 15, 20);
+		expect(d.scale(1/5, 1/5)).toBeRectangle(1, 2, 3/5, 4/5);
+	});
+
+	describe("equals", function() {
+		it("other Rectangle", function(){
+			expect(b.equals(a)).toBeFalsy();
+			expect(b.equals(d)).toBeTruthy();
+			expect(b.equals(new Rectangle(1.000000000001, 2.000000000001,
+											3, 4.000000000001), epsilon)).toBeTruthy();
+		});
+		it("two Vector2's", function() {
+			expect(b.equals(new Vector2(1, 2), new Vector2(3, 4))).toBeTruthy();
+			expect(b.equals(new Vector2(-1, 2), new Vector2(3, 4))).toBeFalsy();
+			expect(b.equals(new Vector2(1, 2), new Vector2(-3, 4))).toBeFalsy();
+			expect(b.equals(new Vector2(1.000000000001, 2.000000000001),
+							new Vector2(3.000000000001, 4.000000000001), epsilon)).toBeTruthy();
+		});
+		it("one Vector2, two scalars", function() {
+			expect(b.equals(new Vector2(1, 2), 3, 4)).toBeTruthy();
+			expect(b.equals(new Vector2(-1, 2), 3, 4)).toBeFalsy();
+			expect(b.equals(new Vector2(1, 2), -3, 4)).toBeFalsy();
+			expect(b.equals(new Vector2(1.000000000001, 2.000000000001),
+							3.000000000001, 4.000000000001, epsilon)).toBeTruthy();
+		});
+		it("four scalars", function() {
+			expect(b.equals(1, 2, 3, 4)).toBeTruthy();
+			expect(b.equals(-1, 2, 3, 4)).toBeFalsy();
+			expect(b.equals(1, 2, -3, 4)).toBeFalsy();
+			expect(b.equals(1.000000000001, 2.000000000001,
+							3.000000000001, 4.000000000001, epsilon)).toBeTruthy();
+		});
+	});
+
+	it("perimeter", function() {
+		expect(a.perimeter()).toBe(0);
+		expect(b.perimeter()).toBe(14);
+		expect(c.perimeter()).toBe(8);
+		expect(f.perimeter()).toBe(14);
+	});
+
+	describe("center", function() {
+		it("get", function() {
+			expect(a.center()).toBeVector(0, 0);
+			expect(b.center()).toBeVector(2.5, 4);
+			expect(c.center()).toBeVector(-1, 4);
+			expect(f.center()).toBeVector(2.5, 4);
+		});
+
+		it("set", function() {
+			a.center(new Vector2(4, 5));
+			expect(a).toBeRectangle(4, 5, 0, 0);
+			expect(b.center(0, 0)).toBeRectangle(-1.5, -2, 3, 4);
+			expect(c.center(10, 5)).toBeRectangle(9, 4, 2, 2);
+			expect(f.center(0, 0)).toBeStandardRectangle(-1.5, -2, 3, 4);
+		});
+
+	});
+});
